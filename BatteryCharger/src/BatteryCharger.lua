a73549441258ad28e7d8728576c0e7f871d00d99
Kieran Winfield <kieran.winfield464@gmail.com>
2021-01-07 19:52:35 +0000

    Refactored code, now works with the 2nd weapon

--- BatteryCharger/src/BatteryCharger.lua
+++ BatteryCharger/src/BatteryCharger.lua
@@ -2,8 +2,32 @@
 BatteryCharger = {}
 
 BatteryCharger.name = "BatteryCharger"
+--If the charge left on the weapon is less than the value below, it will charge the weapon
+BatteryCharger.ChargeTheshold = 3
 
+-- We do this check to reduce the amount of times that FindSoulGems in called
+function BatteryCharger.IsWeaponAttack(AttackType)
+    if (AttackType == 5 or AttackType == 6) then
+        return true
+    else
+        return false
+    end
+end
 
+function BatteryCharger.ChargeWeapon(EquipmentSlot)
+    local SoulGemSlot
+
+    if (IsItemChargeable(BAG_WORN, EQUIP_SLOT_MAIN_HAND)) then
+        SoulGemSlot = BatteryCharger.FindSoulGems()
+        --Check its charge value to make sure it super low or depleted
+        if (GetChargeInfoForItem(BAG_WORN, EQUIP_SLOT_MAIN_HAND) < BatteryCharger.ChargeTheshold) then
+            d("Charging item")
+            ChargeItemWithSoulGem(BAG_WORN, EQUIP_SLOT_MAIN_HAND, BAG_BACKPACK, SoulGemSlot)
+        end
+    end
+end
+
+-- Gets the first soul gem found in the player's inventory
 function BatteryCharger:FindSoulGems()
     local PlayerInventory = SHARED_INVENTORY:GenerateFullSlotData(nil,BAG_BACKPACK)
 
@@ -14,31 +38,19 @@ function BatteryCharger:FindSoulGems()
     end
 end
 
-function BatteryCharger:TypeOfAttack(_, _, _,  _, ActionSlotType)
-    local SoulGemSlot
+function BatteryCharger:Main(_, _, _,  _, AttackType)
     local ActiveWeapon, _
     
-    if (ActionSlotType == 5 or ActionSlotType == 6) then
+    if (BatteryCharger.IsWeaponAttack(AttackType)) then
         ActiveWeapon, _ = GetActiveWeaponPairInfo()
 
         if (ActiveWeapon == 1)then
-            if (IsItemChargeable(BAG_WORN, EQUIP_SLOT_MAIN_HAND)) then
-                SoulGemSlot = BatteryCharger.FindSoulGems()
-                --Check its charge value to make sure it super low or depleted
-                if (GetChargeInfoForItem(BAG_WORN, EQUIP_SLOT_MAIN_HAND) < 125) then
-                    d("Charging item")
-                    ChargeItemWithSoulGem(BAG_WORN, EQUIP_SLOT_MAIN_HAND, BAG_BACKPACK, SoulGemSlot)
-                end
-            end
+            BatteryCharger.ChargeWeapon(EQUIP_SLOT_MAIN_HAND)
         end
         if (ActiveWeapon == 2) then
-            if  IsItemChargeable(BAG_WORN, EQUIP_SLOT_BACKUP_MAIN) then
-                --Check its charge value to make sure it super low or depleted
-                d(GetItemLink(BAG_WORN, EQUIP_SLOT_BACKUP_MAIN))
-            end
+            BatteryCharger.ChargeWeapon(EQUIP_SLOT_BACKUP_MAIN)
         end
     end
 end
 
---Register our OnAddOnLoaded function to the event manager
-EVENT_MANAGER:RegisterForEvent(BatteryCharger.name, EVENT_COMBAT_EVENT, BatteryCharger.TypeOfAttack)
+EVENT_MANAGER:RegisterForEvent(BatteryCharger.name, EVENT_COMBAT_EVENT, BatteryCharger.Main)
