d109084689c4e7ff35d32c5ceb729ca2de346ad9
Kieran Winfield <kieran.winfield464@gmail.com>
2021-01-07 19:15:26 +0000

    Now charges your main hand if you are low

--- BatteryCharger/src/BatteryCharger.lua
+++ BatteryCharger/src/BatteryCharger.lua
@@ -3,19 +3,32 @@ BatteryCharger = {}
 
 BatteryCharger.name = "BatteryCharger"
 
+
+function BatteryCharger:FindSoulGems()
+    local PlayerInventory = SHARED_INVENTORY:GenerateFullSlotData(nil,BAG_BACKPACK)
+
+    for _, item in pairs(PlayerInventory) do
+        if (IsItemSoulGem(SOUL_GEM_TYPE_FILLED,BAG_BACKPACK,item.slotIndex)) then
+            return item.slotIndex
+        end
+    end
+end
+
 function BatteryCharger:TypeOfAttack(_, _, _,  _, ActionSlotType)
     local SoulGemSlot
-    local MainHandChargeInformation
-    local OffhandChargeInformation
     local ActiveWeapon, _
     
     if (ActionSlotType == 5 or ActionSlotType == 6) then
         ActiveWeapon, _ = GetActiveWeaponPairInfo()
 
         if (ActiveWeapon == 1)then
-            if IsItemChargeable(BAG_WORN, EQUIP_SLOT_MAIN_HAND) then
+            if (IsItemChargeable(BAG_WORN, EQUIP_SLOT_MAIN_HAND)) then
+                SoulGemSlot = BatteryCharger.FindSoulGems()
                 --Check its charge value to make sure it super low or depleted
-                d(GetItemLink(BAG_WORN, EQUIP_SLOT_MAIN_HAND))
+                if (GetChargeInfoForItem(BAG_WORN, EQUIP_SLOT_MAIN_HAND) < 125) then
+                    d("Charging item")
+                    ChargeItemWithSoulGem(BAG_WORN, EQUIP_SLOT_MAIN_HAND, BAG_BACKPACK, SoulGemSlot)
+                end
             end
         end
         if (ActiveWeapon == 2) then
